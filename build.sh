a38d25fc8fb050743b6704fb0d8fe09540effd4d
Jonathan Cai <caimjonathan@gmail.com>
2023-02-23 00:38:48 -0500

    add to PATH in build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ sudo apt-get update
 sudo apt-get -y install libpq-dev
 cargo install diesel_cli --no-default-features --features postgres
 cargo build --release
+PATH="$PATH:/root/.cargo/bin"
 diesel database setup
