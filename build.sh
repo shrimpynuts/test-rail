000a94ebfdc532052acbe4266b2dc51d1a55ae2c
Jonathan Cai <caimjonathan@gmail.com>
2023-02-23 01:18:44 -0500

    add rustflag

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 sudo apt-get update
 sudo apt-get -y install libpq-dev
 cargo install diesel_cli --no-default-features --features postgres
-cargo build --release
+RUSTFLAGS='-L /usr/local/pgsql/lib' cargo build --release
 PATH="$PATH:/root/.cargo/bin"
 diesel database setup
