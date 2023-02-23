1028b016a2d701a002c16dd8ecf8105b146a7180
Jonathan Cai <caimjonathan@gmail.com>
2023-02-23 01:29:17 -0500

    lord please

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ sudo apt -y install libpq-dev
 sudo apt -y install libgssapi-krb5-2 
 sudo apt -y install libgssapi-krb5-2:i386
 sudo apt -y install rsyslog-gssapi
+sudo apt -y install postgresql-devel
 echo "------------- cargo stuff"
-cargo install diesel_cli --no-default-features --features postgres
+cargo install diesel_cli --no-default-features --features postgres --verbose
 RUSTFLAGS='-L /usr/local/pgsql/lib' cargo build --release
 PATH="$PATH:/root/.cargo/bin"
 diesel database setup
