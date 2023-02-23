f91ad77aca49eb69c62252c3f5763764e8ae8c06
Jonathan Cai <caimjonathan@gmail.com>
2023-02-23 01:24:55 -0500

    add libgssapi install, use apt

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get -y install libpq-dev
+sudo apt update
+sudo apt -y install libpq-dev
+sudo apt -y install libgssapi-krb5-2 
+sudo apt -y install libgssapi-krb5-2:i386
 cargo install diesel_cli --no-default-features --features postgres
 RUSTFLAGS='-L /usr/local/pgsql/lib' cargo build --release
 PATH="$PATH:/root/.cargo/bin"
