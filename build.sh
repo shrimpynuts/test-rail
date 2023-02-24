410254d1d451d27c47b5af613aa79ae0d5f82ce6
Jonathan Cai <caimjonathan@gmail.com>
2023-02-23 19:35:10 -0500

    symlink

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ sudo apt -y install rsyslog-gssapi
 sudo apt -y install postgresql-devel
 echo "------------- apt-cache search libpq"
 apt-cache search libpq
+echo "sudo ln -s  /usr/local/pgsql/lib/* /usr/lib/"
+sudo ln -s  /usr/local/pgsql/lib/* /usr/lib/
 echo "------------- cargo stuff"
 cargo install diesel_cli --no-default-features --features postgres --verbose
 RUSTFLAGS='-L /usr/local/pgsql/lib' cargo build --release
